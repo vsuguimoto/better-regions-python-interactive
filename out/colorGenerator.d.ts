99c763da65b624f696a4d6203388e2876bd8ea7e
Vinicius Suguimoto <vinisuguimoto@gmail.com>
2026-03-28 12:08:28 -0300

    chore: remove node_modules, out, and package-lock.json from tracking

--- out/colorGenerator.d.ts
+++
@@ -1,6 +0,0 @@
-export interface GeneratedColors {
-    background: string;
-    border: string;
-}
-export declare function generateRegionColors(name: string, depth: number, saturation: number, baseLightness: number, opacity: number): GeneratedColors;
-//# sourceMappingURL=colorGenerator.d.ts.map
